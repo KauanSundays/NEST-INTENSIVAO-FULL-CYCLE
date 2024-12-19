9c8aa94d9a473c655aa5775c1564fab6300aef99
Kauan <kauan.rocha@ondacom.com>
2024-12-19 07:39:48 -0300

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import { Module, Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { ProcessosController } from './processos/processos.controller';
+
+@Controller('prefixo')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get('test')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('test1')
+  acao(): string {
+    return 'Kauanzito';
+  }
+}
+
+@Module({
+  imports: [],
+  controllers: [AppController, ProcessosController],
+  providers: [AppService],
+})
+export class AppModule {}
